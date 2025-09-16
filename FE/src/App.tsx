b8ed3cb6c38b15156084d095cc614f67b8932d8a
JoeSamirFaik <jojo.jesus3512@gmail.com>
2025-09-16 06:46:10 +0300

    Updated App.tsx

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,31 +1,34 @@
-@@ .. @@
- import React from 'react';
--import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
- import { AuthProvider } from './contexts/AuthContext';
- import { LoginPage } from './pages/LoginPage';
- import { Dashboard } from './pages/Dashboard';
-+import { AdminDashboard } from './pages/admin/AdminDashboard';
- import { ProtectedRoute } from './components/ProtectedRoute';
- 
- function App() {
-@@ .. @@
-           <Routes>
-             <Route path="/login" element={<LoginPage />} />
-             <Route path="/dashboard" element={
-               <ProtectedRoute>
-                 <Dashboard />
-               </ProtectedRoute>
-             } />
-+            <Route path="/admin" element={
-+              <ProtectedRoute>
-+                <AdminDashboard />
-+              </ProtectedRoute>
-+            } />
-             <Route path="/" element={<Navigate to="/dashboard" replace />} />
-           </Routes>
-         </Router>
-       </AuthProvider>
-     </div>
-   );
- }
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { LoginPage } from './pages/LoginPage';
+import { Dashboard } from './pages/Dashboard';
+import { AdminDashboard } from './pages/admin/AdminDashboard';
+import { ProtectedRoute } from './components/ProtectedRoute';
+
+function App() {
+  return (
+    <div className="App">
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/dashboard" element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            } />
+            <Route path="/admin" element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            } />
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
